3fd72bf5c3658c80aeb499f7e015bfec5915adff
Azat Burkhanov <azburhanov@mail.ru>
2022-02-23 18:15:02 +0300

    repo update

--- src/main/java/com/stacktoheap/maven_ci_example/App.java
+++ src/main/java/com/stacktoheap/maven_ci_example/App.java
@@ -2,12 +2,9 @@ package com.stacktoheap.maven_ci_example;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Maven CI with TeamCity!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven CI with TeamCity!");
     }
 }
